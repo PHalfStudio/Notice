8d9ed42c28133e38f9fc622df6e21082f32d09b0
Natsume <phalfstudio@gmail.com>
2023-06-27 19:50:29 +0800

    Dev Daily commit 23-06-27

--- app/src/main/java/pro/phalfstudio/notice/net/CheckUpdate.java
+++ app/src/main/java/pro/phalfstudio/notice/net/CheckUpdate.java
@@ -61,57 +61,39 @@ public class CheckUpdate {
                     description = null;
                 }
                 if (date != null && longVersion != null && dlUrl != null && description != null) {
-                    if (userCheck) {
-                        appLongVersion = context.getString(R.string.number_version_code);
-                        if (Integer.parseInt(longVersion) > Integer.parseInt(appLongVersion)) {
-                            int i = Integer.parseInt(longVersion.substring(7));
-                            System.out.println(i);
-                            AlertDialog dialog = new AlertDialog.Builder(context)
-                                    .setTitle(appVersion+"版本更新")
-                                    .setMessage("检测到新版本！更新信息如下："+description+"\n\n更新时间："+date)
-                                    .setCancelable(false)
-                                    .setPositiveButton("更新", (dialog1, which) -> {
-                                        Intent openIntent = new Intent(Intent.ACTION_VIEW);
-                                        openIntent.setData(Uri.parse(dlUrl));
-                                        context.startActivity(openIntent);
-                                    })
-                                    .setNegativeButton("取消", (dialog2, which) -> {
-                                        System.out.println(i);
-                                        if(i == 0){
-                                            dialog2.dismiss();
-                                        }else if(i == 1){
-                                            Toast.makeText(context, "本版本为强制更新，请更新至最新版本！", Toast.LENGTH_SHORT).show();
-                                        }
-                                    })
-                                    .create();
-                            dialog.show();
-                        }else{
-                            Toast.makeText(context, "应用为最新版本！", Toast.LENGTH_SHORT).show();
-                        }
-                    }else{
-                        appLongVersion = context.getString(R.string.number_version_code);
-                        if (Integer.parseInt(longVersion) > Integer.parseInt(appLongVersion)) {
-                            int i = Integer.parseInt(longVersion.substring(7));
-                            AlertDialog dialog = new AlertDialog.Builder(context)
-                                    .setTitle(appVersion+"版本更新")
-                                    .setMessage("检测到新版本！更新信息如下："+"\n\n"+description+"\n\n更新时间："+date)
-                                    .setCancelable(false)
-                                    .setPositiveButton("更新", (dialog1, which) -> {
-                                        Intent openIntent = new Intent(Intent.ACTION_VIEW);
-                                        openIntent.setData(Uri.parse(dlUrl));
-                                        context.startActivity(openIntent);
-                                    })
-                                    .setNegativeButton("取消", (dialog2, which) -> {
-                                        if(i == 0){
-                                            dialog2.dismiss();
-                                        }else if(i == 1){
-                                            Toast.makeText(context, "本版本为强制更新，请更新至最新版本！", Toast.LENGTH_SHORT).show();
-                                        }
-                                    })
-                                    .create();
-                            dialog.show();
+                    AlertDialog.Builder alertStrong = new AlertDialog.Builder(context);
+                    AlertDialog.Builder alertNormal = new AlertDialog.Builder(context);
+                    alertStrong.setTitle(appVersion + " 版本更新")
+                            .setMessage("检测到强制更新版本！\n\n" + description + "\n\n更新时间：" + date)
+                            .setCancelable(false)
+                            .setPositiveButton("更新", (dialog1, which) -> {
+                                Intent openIntent = new Intent(Intent.ACTION_VIEW);
+                                openIntent.setData(Uri.parse(dlUrl));
+                                context.startActivity(openIntent);
+                            })
+                            .create();
+                    alertNormal.setTitle(appVersion + " 版本更新")
+                            .setMessage("检测到新版本！更新信息如下：\n\n" + description + "\n\n更新时间：" + date)
+                            .setCancelable(false)
+                            .setPositiveButton("更新", (dialog1, which) -> {
+                                Intent openIntent = new Intent(Intent.ACTION_VIEW);
+                                openIntent.setData(Uri.parse(dlUrl));
+                                context.startActivity(openIntent);
+                            })
+                            .setNegativeButton("取消", (dialog2, which) -> {
+                                dialog2.dismiss();
+                            })
+                            .create();
+                    int i = Integer.parseInt(longVersion.substring(7));
+                    appLongVersion = context.getString(R.string.number_version_code);
+                    if (Integer.parseInt(longVersion) > Integer.parseInt(appLongVersion)) {
+                        if (i == 1) {
+                            alertStrong.show();
+                        } else {
+                            alertNormal.show();
                         }
                     }
+
                 }
             }
 
